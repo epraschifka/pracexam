834a36567d7a252be32c7b4aa95545f01cd0f4da
epraschifka <mredward@live.com.au>
2022-10-27 14:07:49 +1030

    committed

--- AirCraft.cpp
+++ AirCraft.cpp
@@ -1,6 +1,10 @@
 #include "AirCraft.h"
 AirCraft::AirCraft() {}
-AirCraft::AirCraft(int w) {weight = w;}           // creates an Aircraft with weight w
+AirCraft::AirCraft(int w) {
+    weight = w;
+    fuel = 100;
+    numberOfFlights = 0;
+    }           // creates an Aircraft with weight w
 void AirCraft::refuel() {fuel = 100;}             // Resets fuel back to 100%
 void AirCraft::set_weight(int _weight) {weight = _weight;}
 void AirCraft::set_fuel(float _fuel) {fuel = _fuel;}
